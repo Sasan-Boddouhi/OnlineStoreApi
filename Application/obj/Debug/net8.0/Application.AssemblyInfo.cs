6decc1f488bfe9bb7ca1400d08bd403896767cce
Sasan-Boddouhi <sasan.boddouhi@gmail.com>
2026-05-31 11:58:58 +0330

    fix: remove AutoMapper.Extensions.Microsoft.DependencyInjection package to resolve ambiguity

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e46056d74796180ddbb055b1408a0dacfa3682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e086e2dd936edebbc0100ba7aebbfa85dfb498a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
